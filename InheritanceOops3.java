b3610b7371f100ac7c35c90ebdc8c9b61ec0cc72
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-15 21:52:07 +0530

    Update InheritanceOops3.java

--- InheritanceOops3.java
+++ InheritanceOops3.java
@@ -5,6 +5,8 @@ class Shape {
 }
 class Triangle extends Shape {
    public void area(int h, int b) {
+      int h=10,int=12;
        System.out.println((1/2)*b*h);
+      // Something error..
    }  
 }
